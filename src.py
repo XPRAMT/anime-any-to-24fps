adfd28b36e5046703fde86891624e0a88321bbc7
XPRAMT <todo635387@gmail.com>
2024-01-17 23:15:28 +0800

    Update src.py

--- src.py
+++ src.py
@@ -2,28 +2,29 @@
 from fractions import Fraction
 from operator import itemgetter
 import ffmpeg
+import os
 from joblib import Parallel, delayed
-import multiprocessing
-num_cores = multiprocessing.cpu_count() - 2
-################################
+############自訂參數############
+num_cores = 14                  #多執行序
+input_args = {
+    "hwaccel": "nvdec",         #硬體解碼
+}
+output_args = {
+    "vcodec": "hevc_nvenc",     # 使用編碼器
+    "preset": "slow",           # 編碼速度:fast,medium,slow,
+    "profile:v": "main10",      # 設置主要配置:main10
+    "pix_fmt": "p010le",        # 設置像素格式:yuv420p,p010le
+    "rc-lookahead": 16,         # 設置編碼器的預測深度
+    "cq": 15,                   # 設置CQP模式的量化參數(QP)
+}
+pipe_fmt='yuv444p'              #rgb24,yuv444p,p010le...(rgb可能導致色彩偏移)
+############全局參數############
+OutputName = ''     
 frate = ''
 width = ''
 height = ''
 num_frames = ''
-Remove_frames = []
 Even = 1
-input_args = {
-    "hwaccel": "nvdec",
-}
-output_args = {
-    "vcodec": "hevc_nvenc",     # 使用 hevc_nvenc 編碼器進行 HEVC 編碼
-    "preset": "medium",         # 選擇適當的預設，例如 "slow"
-    "profile:v": "main10",      # 選擇 10 位色深的主要配置
-    "pix_fmt": "p010le",        # 設置像素格式為 10 位 yuv420p
-    "rc-lookahead": 16,         # 設置編碼器的預測深度
-    "cq": 20,                   # 設置 CQP 模式的量化參數（QP）
-}
-
 ##############函式##############
 def GetVideoInfo(VieoPath):
     global frate,width,height,num_frames,colors
@@ -38,9 +39,9 @@ def GetVideoInfo(VieoPath):
         num_frames = int(tags.get('NUMBER_OF_FRAMES', 0))
     if 'color_space' in v_stream:
         output_args["colorspace"] = v_stream.get('color_space')
-    if 'color_space' in v_stream:
-        output_args["color_transfer"] = v_stream.get('color_transfer')
-    if 'color_space' in v_stream:
+    if 'color_transfer' in v_stream:
+        output_args["color_trc"] = v_stream.get('color_transfer')
+    if 'color_primaries' in v_stream:
         output_args["color_primaries"] = v_stream.get('color_primaries')
     print("-----------input-----------")
     print(v_stream)
@@ -53,45 +54,52 @@ def ImageSimilarity(image1, image2):
     degree = np.mean(Difference)
     return degree
     
-def Remove(decoder):#30->24
-    global Even,frate,width,height,num_frames,colors
+def Remove(video,audio):#30->24
+    global Even,frate,width,height,num_frames,OutputName
     encoder = (
         ffmpeg
-        .input('pipe:', framerate='{}'.format(frate), format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width,height))
-        .output('output.mkv',**output_args)
+        .input('pipe:', framerate='{}'.format(frate), format='rawvideo', pix_fmt=pipe_fmt, s='{}x{}'.format(width,height))
+        .output(audio,OutputName,**output_args)
         .overwrite_output()
         .run_async(pipe_stdin=True)
         )
 
     s=np.zeros((5,2))#索引,相似度
-    in_bytes = decoder.stdout.read(width * height * 3)
-    img0 = (np.frombuffer(in_bytes, np.uint8).reshape([height, width, 3]))
-    imglist=[]
+    in_bytes = video.stdout.read(width * height * 3)#讀取第0幀
+    img0 = np.frombuffer(in_bytes, np.uint8)
+    imglist=[]#圖片序列
     imglist.append(img0)
+    write_frames=[]#寫入清單
+    k=0
     for i in range(1,num_frames):
         #讀取圖片
-        in_bytes = decoder.stdout.read(width * height * 3)
-        frame = (np.frombuffer(in_bytes, np.uint8).reshape([height, width, 3]))
+        in_bytes = video.stdout.read(width * height * 3)
+        frame = np.frombuffer(in_bytes, np.uint8)
+        #寫入圖片序列
         if(i%Even==0):
+            write_frames.append(k)
             imglist.append(frame)
-
+            k+=1
+        #比較排序圖片
         if(i%(5*Even)==0):
             imglist=np.array(imglist)
-            write_frames=np.array([0,1,2,3,4])
             #比較圖片
             def compute_similarity(j):
                 nonlocal imglist
                 return ImageSimilarity(imglist[j], imglist[j + 1])
-            result = Parallel(n_jobs=num_cores)(delayed(compute_similarity)(i) for i in range(5))
+            result = Parallel(num_cores)(delayed(compute_similarity)(i) for i in range(5))
             for j in range(5):
                 s[j,0] = j
                 s[j,1] = result[j]
             #排序
             s2 = sorted(s,key = itemgetter(1))
             s2 = np.array(s2,dtype=int)
-            print(s2)
-            write_frames = write_frames[write_frames != s2[0,0]]
-            #寫入
+            #從寫入清單移除
+            write_frames.remove(s2[0,0])
+            Remove_frame = i + s2[0,0]*Even
+        #寫入
+        if(i%(5*Even)==0) or (i==num_frames):
+            write_frames=np.array(write_frames)
             for index in write_frames:
                 encoder.stdin.write(
                             imglist[index]
@@ -100,31 +108,43 @@ def Remove(decoder):#30->24
                         )
             #顯示狀態
             realindex = i + write_frames*Even
-            print(f'{i} in {num_frames} |保存:{realindex}')
-            temp=imglist[5]
-            imglist=[]
-            imglist.append(temp)
+            print(f'{i} in {num_frames} |移除幀:{Remove_frame} |保存幀:{realindex}')
+            #重置
+            if(i!=num_frames):
+                temp=imglist[5]
+                imglist=[]
+                imglist.append(temp)
+                write_frames=[]
+                Remove_frame='None'
+                k=0
         
-    encoder.stdin.close()
+    encoder.stdin.close() #關閉影片流
         
-
 ##############Main##############
 def Main(path):
-    global frate,Remove_frames,num_frames,Even
-    decoder = (
-        ffmpeg.input(path,**input_args)
-        .output('pipe:', format='rawvideo', pix_fmt='rgb24', vframes=num_frames)
+    global frate,num_frames,Even,OutputName
+    #取得輸出檔名
+    fileName, _ = os.path.splitext(os.path.basename(path))
+    OutputName = f"{fileName}_fix{'.mkv'}"
+    #分離影片聲音流
+    input=ffmpeg.input(path,**input_args)
+    video = (
+        input
+        .output('pipe:', format='rawvideo', pix_fmt=pipe_fmt, vframes=num_frames)
         .run_async(pipe_stdout=True)
     )
-
+    audio = input.audio
+    
     if (59<frate<61):
         Even = 2
         frate /= 2
 
     if (29<frate<31):
         frate = frate*4/5
-        Remove(decoder)
-    else:
+        Remove(video,audio)
+    elif(23<frate<25):
         print("不需要處理")
+    else:
+        print("例外情況")
 
-    
+    print("處理完成!")
