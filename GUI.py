691935a19e0486c04df786b7d2601e5154a60d4b
XPRAMT <todo635387@gmail.com>
2024-01-19 05:24:04 +0800

    支援任務序列

--- GUI.py
+++ GUI.py
@@ -3,37 +3,42 @@ from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 import sys
 import src
+import threading
+import queue
 
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
         #初始化變數
         self.filePath = ""
-        # 初始化視窗介面
-        self.initUI()
-
-    def initUI(self):
+        self.task_num = 0
+        # 設定視窗的幾何屬性（位置和大小）
+        self.setGeometry(0, 0, 600, 300)
+        self.center()
+        # 設定視窗的標題
+        self.setWindowTitle('移除多餘幀')
         # 建立開始按鈕
         button_star = QPushButton('開始!', self)
         button_star.clicked.connect(self.buttonStarClicked)
         # 建立選擇按鈕
         button_Sel = QPushButton('選擇檔案', self)
         button_Sel.clicked.connect(self.selectFile)
-        # 建立文字框
-        self.label = QLabel()
+        # 建立任務清單
+        self.list_widget = QListWidget()
         # 建立一個垂直佈局管理器，並將按鈕新增至佈局中
-        vbox = QVBoxLayout()
-        vbox.addWidget(button_Sel)
-        vbox.addWidget(button_star)
-        vbox.addWidget(self.label)
-        # 將佈局設定為視窗的主佈局
-        self.setLayout(vbox)
-        # 設定視窗的幾何屬性（位置和大小）
-        self.setGeometry(0, 0, 600, 300)
-        self.center()
-        # 設定視窗的標題
-        self.setWindowTitle('移除多餘幀')
-
+        self.vbox = QVBoxLayout()
+        #self.vbox.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.setLayout(self.vbox)# 將佈局設定為視窗的主佈局
+        self.vbox.addWidget(button_Sel)
+        self.vbox.addWidget(button_star)
+        self.vbox.addWidget(self.list_widget)
+        # 添加清單項目
+        self.list_widget.addItem('')
+        # 更新狀態線程
+        t2 = threading.Thread(target=self.updateChanged)
+        t2.daemon = True
+        t2.start()
+    #視窗置中
     def center(self):
             # 取得螢幕的幾何訊息
             screenGeometry = QApplication.primaryScreen().geometry()
@@ -42,59 +47,73 @@ class MyWindow(QWidget):
             y = (screenGeometry.height() - self.height()) // 2
             # 設定視窗的位置
             self.setGeometry(x, y, self.width(), self.height())
-
-    # 自訂的按鈕點擊事件處理方法
+    # 開始按鈕
     def buttonStarClicked(self):
-        src.Main(self.filePath)
-        #self.label.setText(f'<p style="color:white;">處理中</p>')
-        
-    def selectFolder(self):# 建立資料夾選擇對話框
-        self.folder_path = QFileDialog.getExistingDirectory(self, 'Select Folder')
-        self.label.setText(f'<p style="color:white;">folder path:{self.folder_path}</p>')
-    
-    def selectFile(self):# 建立檔案選擇對話框
-        self.filePath,_ = QFileDialog.getOpenFileName(self, 'Select File')
-        self.label.setText(f'<p style="color:white;">file path:{self.filePath}</p>')
+        current_item = self.list_widget.item(self.task_num)
         if(self.filePath):
-            src.GetVideoInfo(self.filePath)
-
+            self.SetText(current_item,0,self.filePath)
+            # 放進任務序列
+            process_queue.put([self.filePath,self.task_num])
+            # 重置路徑
+            self.filePath=''
+            # 建立新的清單
+            self.task_num+=1
+            self.list_widget.addItem('')
+        else:
+            current_item.setText('請先選擇檔案')
+    # 建立檔案選擇對話框
+    def selectFile(self):
+        self.filePath,_ = QFileDialog.getOpenFileName(self, 'Select File')
+        current_item = self.list_widget.item(self.task_num)
+        current_item.setText(f'{self.filePath}')
+        if(self.filePath):
+           src.GetVideoInfo(self.filePath)
+    # 更新狀態  
+    def updateChanged(self):
+        while (True):
+            parameter = state_queue.get() # 等待狀態更新
+            self.SetText(self.list_widget.item(parameter[1]),parameter[2],parameter[0])
+    # 設定文字
+    def SetText(self,item,case,path):
+        match case:
+            case 0:
+                combined_text = path + ' | 等待中...'
+                item.setText(combined_text)
+                item.setForeground(QColor(255, 0, 0))
+            case 1:
+                combined_text = path + ' | 處理中...'
+                item.setText(combined_text)
+                item.setForeground(QColor(255, 255, 0))
+            case 2:
+                combined_text = path + ' | 完成!'
+                item.setText(combined_text)
+                item.setForeground(QColor(0, 200, 0))
 
 # 當腳本作為主程式執行時
 if __name__ == '__main__':
-    # 建立 QApplication 實例
+    process_queue = queue.Queue() # 任務
+    state_queue = queue.Queue()   # 狀態
+    # 建立線程
+    t = threading.Thread(target=src.Main,args=(process_queue,state_queue,))
+    t.daemon = True 
+    t.start()
+    # 建立GUI
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
+    # 設定深色主題
     dark_palette = QPalette()
     dark_palette.setColor(QPalette.ColorRole.Window, QColor(0, 0, 0))
     dark_palette.setColor(QPalette.ColorRole.Button, QColor(20, 20, 20))
     app.setPalette(dark_palette)
+    # 設定字體
+    default_font = QFont('Microsoft JhengHei',12)
+    app.setFont(default_font)
     # 建立 MyWindow 實例
     window = MyWindow()
     # 顯示視窗
     window.show()
     # 運行應用程式的主循環
     sys.exit(app.exec())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- src.py
+++ src.py
@@ -3,13 +3,15 @@ from fractions import Fraction
 from operator import itemgetter
 import ffmpeg
 import os
-from joblib import Parallel, delayed
+import threading
+import time
+import copy
 ############自訂參數############
 num_cores = 14                  #多執行序
 input_args = {
     "hwaccel": "nvdec",         #硬體解碼
 }
-output_args = {
+defual_output_args = {
     "vcodec": "hevc_nvenc",     # 使用編碼器
     "preset": "slow",           # 編碼速度:fast,medium,slow,
     "profile:v": "main10",      # 設置主要配置:main10
@@ -18,16 +20,11 @@ output_args = {
     "cq": 15,                   # 設置CQP模式的量化參數(QP)
 }
 pipe_fmt='yuv444p'              #rgb24,yuv444p,p010le...(rgb可能導致色彩偏移)
-############全局參數############
-OutputName = ''     
-frate = ''
-width = ''
-height = ''
-num_frames = ''
-Even = 1
 ##############函式##############
+print_info = True
 def GetVideoInfo(VieoPath):
-    global frate,width,height,num_frames,colors
+    global defual_output_args,print_info
+    output_args = copy.deepcopy(defual_output_args)
     probe = ffmpeg.probe(VieoPath)
     v_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
     width = int(v_stream['width'])
@@ -43,108 +40,133 @@ def GetVideoInfo(VieoPath):
         output_args["color_trc"] = v_stream.get('color_transfer')
     if 'color_primaries' in v_stream:
         output_args["color_primaries"] = v_stream.get('color_primaries')
-    print("-----------input-----------")
-    print(v_stream)
-    print("-----------output-----------")
-    print(output_args)
-    return frate
+    if print_info:
+        print("-----------input-----------")
+        print(v_stream)
+        print("-----------output-----------")
+        print(output_args)
+    return frate,num_frames,width,height,output_args
 
 def ImageSimilarity(image1, image2):
     Difference = abs(image2 - image1)
     degree = np.mean(Difference)
     return degree
-    
-def Remove(video,audio):#30->24
-    global Even,frate,width,height,num_frames,OutputName
-    encoder = (
-        ffmpeg
-        .input('pipe:', framerate='{}'.format(frate), format='rawvideo', pix_fmt=pipe_fmt, s='{}x{}'.format(width,height))
-        .output(audio,OutputName,**output_args)
-        .overwrite_output()
-        .run_async(pipe_stdin=True)
-        )
 
-    s=np.zeros((5,2))#索引,相似度
+def compare_sort_write(parameters):
+    encoder,imglist,write_frames,i2,isEnd,num_frames,Even = parameters
+    num_list=len(write_frames)
+    i1=i2-num_list*Even
+    #如果不足5幀補一幀
+    if num_list<5:
+        write_frames.append(write_frames[-1]+1)
+        imglist.append(np.zeros_like(imglist[0]))
+    s = np.zeros((num_list, 2))#索引,相似度
+    imglist=np.array(imglist)
+    #比較圖片
+    for j in range(num_list):
+        s[j,0] = j
+        s[j,1] = ImageSimilarity(imglist[j], imglist[j + 1])
+
+    #排序
+    s2 = sorted(s,key = itemgetter(1))
+    s2 = np.array(s2,dtype=int)
+    #從寫入清單移除
+    write_frames.remove(s2[0,0])
+    Remove_frame = i1 + s2[0,0]*Even
+    #寫入
+    write_frames=np.array(write_frames)
+    for index in write_frames:
+        encoder.stdin.write(imglist[index].astype(np.uint8).tobytes())
+    if isEnd:
+        encoder.stdin.close() #關閉影片流
+    #顯示狀態
+    realindex = i1 + write_frames*Even
+    print(f'frame:{i1}-{i2},All:{num_frames} |移除幀:{Remove_frame} |保存幀:{realindex}')
+
+def Remove(video,encoder,width,height,num_frames,Even):#30->24
     in_bytes = video.stdout.read(width * height * 3)#讀取第0幀
-    img0 = np.frombuffer(in_bytes, np.uint8)
+    img1 = np.frombuffer(in_bytes, np.uint8)
     imglist=[]#圖片序列
-    imglist.append(img0)
+    imglist.append(img1)
     write_frames=[]#寫入清單
+    read_frame=''
     k=0
-    for i in range(1,num_frames):
+    for i in range(2,num_frames+1):
         #讀取圖片
         in_bytes = video.stdout.read(width * height * 3)
         frame = np.frombuffer(in_bytes, np.uint8)
         #寫入圖片序列
-        if(i%Even==0):
+        if(i%Even==1):
             write_frames.append(k)
             imglist.append(frame)
+            read_frame=i
             k+=1
-        #比較排序圖片
-        if(i%(5*Even)==0):
-            imglist=np.array(imglist)
-            #比較圖片
-            def compute_similarity(j):
-                nonlocal imglist
-                return ImageSimilarity(imglist[j], imglist[j + 1])
-            result = Parallel(num_cores)(delayed(compute_similarity)(i) for i in range(5))
-            for j in range(5):
-                s[j,0] = j
-                s[j,1] = result[j]
-            #排序
-            s2 = sorted(s,key = itemgetter(1))
-            s2 = np.array(s2,dtype=int)
-            #從寫入清單移除
-            write_frames.remove(s2[0,0])
-            Remove_frame = i + s2[0,0]*Even
-        #寫入
-        if(i%(5*Even)==0) or (i==num_frames):
-            write_frames=np.array(write_frames)
-            for index in write_frames:
-                encoder.stdin.write(
-                            imglist[index]
-                            .astype(np.uint8)
-                            .tobytes()
-                        )
-            #顯示狀態
-            realindex = i + write_frames*Even
-            print(f'{i} in {num_frames} |移除幀:{Remove_frame} |保存幀:{realindex}')
+        #比較 排序 寫入
+        isEnd = (i==num_frames)
+        if(k==5) or isEnd:
+            parameters = encoder,imglist,write_frames,read_frame,isEnd,num_frames,Even
+            t = threading.Thread(target=compare_sort_write,args=(parameters,))
+            t.start()
             #重置
             if(i!=num_frames):
                 temp=imglist[5]
                 imglist=[]
                 imglist.append(temp)
                 write_frames=[]
-                Remove_frame='None'
                 k=0
         
-    encoder.stdin.close() #關閉影片流
-        
 ##############Main##############
-def Main(path):
-    global frate,num_frames,Even,OutputName
-    #取得輸出檔名
-    fileName, _ = os.path.splitext(os.path.basename(path))
-    OutputName = f"{fileName}_fix{'.mkv'}"
-    #分離影片聲音流
-    input=ffmpeg.input(path,**input_args)
-    video = (
-        input
-        .output('pipe:', format='rawvideo', pix_fmt=pipe_fmt, vframes=num_frames)
-        .run_async(pipe_stdout=True)
-    )
-    audio = input.audio
-    
-    if (59<frate<61):
-        Even = 2
-        frate /= 2
+def Main(process_queue,state_queue):
+    global print_info
+    while (True):
+        parameter = process_queue.get()#等待任務
+        parameter.append(1)
+        state_queue.put(parameter) #返回處理狀態
+        #取得輸出檔名
+        path = parameter[0]
+        print_info = False
+        frate,num_frames,width,height,output_args = GetVideoInfo(path)
+        print_info = True
+        fileName, _ = os.path.splitext(os.path.basename(path))
+        OutputName = f"{fileName}_fix{'.mkv'}"
+        #分離影片聲音流
+        input=ffmpeg.input(path,**input_args)
+        video = (
+            input
+            .output('pipe:', format='rawvideo', pix_fmt=pipe_fmt, vframes=num_frames)
+            .run_async(pipe_stdout=True)
+        )
+        audio = input.audio
 
-    if (29<frate<31):
-        frate = frate*4/5
-        Remove(video,audio)
-    elif(23<frate<25):
-        print("不需要處理")
-    else:
-        print("例外情況")
+        Even = 1
+        if (59<frate<61):
+            Even = 2
+            frate /= 2
+        if (29<frate<31):
+            frate = frate*4/5
+            #建立編碼器
+            encoder = (
+                ffmpeg
+                .input('pipe:', framerate='{}'.format(frate)
+                        , format='rawvideo', pix_fmt=pipe_fmt
+                        , s='{}x{}'.format(width,height)
+                        ,thread_queue_size=128,)
+                .output(audio,OutputName,**output_args)
+                .overwrite_output()
+                .run_async(pipe_stdin=True)
+                )
+            Remove(video,encoder,width,height,num_frames,Even)
+            time.sleep(1)
+            print(f"處理完成! | 幀率:{frate:.3f}")
+        elif(23<frate<25):
+            time.sleep(0.5)
+            print(f"不需要處理 | 幀率:{frate:.3f}")
+        else:
+            time.sleep(0.5)
+            print(f"例外情況 | 幀率:{frate:.3f}")
+        #返回結束狀態
+        parameter[2] = 2
+        state_queue.put(parameter)
 
-    print("處理完成!")
+
+
